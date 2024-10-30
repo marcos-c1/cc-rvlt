0ff8f8718482869b122246c7fe7bb3539df8a756
Marcos <marcos.b.campos14@gmail.com>
2024-10-30 18:20:29 -0300

    feat(login): login template page

--- js/home.js
+++ js/home.js
@@ -4,38 +4,34 @@ const courses_btn = document.getElementById("courses_btn");
 const prices_btn = document.getElementById("prices_btn");
 const login_btn = document.getElementById("login_btn");
 
-login_btn.addEventListener('mouseenter', () => {
-    login_btn.children[0].children[0].style.fill = "var(--orange)";
-})
+login_btn.addEventListener("mouseenter", () => {
+  login_btn.children[0].children[0].style.fill = "var(--orange)";
+});
 
-login_btn.addEventListener('mouseleave', () => {
-    login_btn.children[0].children[0].style.fill = "var(--white)";
-})
+login_btn.addEventListener("mouseleave", () => {
+  login_btn.children[0].children[0].style.fill = "var(--white)";
+});
 
-courses.addEventListener('mouseenter', () => {
-    for(let i = 0; i < courses_btn.children[0].childElementCount; i++){
-        courses_btn.children[0].children[i].style.fill = "var(--white)"
-        
-    }
+courses.addEventListener("mouseenter", () => {
+  for (let i = 0; i < courses_btn.children[0].childElementCount; i++) {
+    courses_btn.children[0].children[i].style.fill = "var(--white)";
+  }
+});
 
-})
+prices.addEventListener("mouseenter", () => {
+  for (let i = 0; i < courses_btn.children[0].childElementCount; i++) {
+    prices_btn.children[0].children[i].style.fill = "var(--white)";
+  }
+});
 
-prices.addEventListener('mouseenter', () => {
-    for(let i = 0; i < courses_btn.children[0].childElementCount; i++){
-        prices_btn.children[0].children[i].style.fill = "var(--white)"
-    }
-})
+courses.addEventListener("mouseleave", () => {
+  for (let i = 0; i < courses_btn.children[0].childElementCount; i++) {
+    courses_btn.children[0].children[i].style.fill = "var(--orange)";
+  }
+});
 
-courses.addEventListener('mouseleave', () => {
-    for(let i = 0; i < courses_btn.children[0].childElementCount; i++){
-        courses_btn.children[0].children[i].style.fill = "var(--orange)"
-        
-    }
-})
-
-prices.addEventListener('mouseleave', () => {
-    for(let i = 0; i < courses_btn.children[0].childElementCount; i++){
-        prices_btn.children[0].children[i].style.fill = "var(--orange)"
-    }
-})
-
+prices.addEventListener("mouseleave", () => {
+  for (let i = 0; i < courses_btn.children[0].childElementCount; i++) {
+    prices_btn.children[0].children[i].style.fill = "var(--orange)";
+  }
+});
