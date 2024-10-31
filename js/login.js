160c26c83a2ed1dfbb71bfe88fa6d80f8079a92b
Marcos <marcos.b.campos14@gmail.com>
2024-10-31 00:39:00 -0300

    feat(login): opacity blur bg on forgot pwd modal

---
+++ js/login.js
@@ -0,0 +1,38 @@
+const loginForm = document.getElementById("login__container");
+const rememberSpan = document.querySelector("#remember__container > span");
+const checkbox = document.getElementById("check");
+const forgotPwd = document.getElementById("forgot-pwd");
+const forgotModal = document.getElementById("forgot-pwd-modal");
+const bgBlur = document.getElementById("opacity-bg");
+
+//TODO: Implement an onclick event outside forgotModal container that closes the container itself
+forgotPwd.addEventListener("click", () => {
+  console.log(forgotModal.style.display);
+  if (forgotModal.style.display === "none" || !forgotModal.style.display) {
+    bgBlur.removeAttribute("hidden");
+    forgotModal.style.display = "block";
+  } else {
+    bgBlur.setAttribute("hidden");
+    forgotModal.style.display = "none";
+  }
+});
+
+rememberSpan.addEventListener("click", () => {
+  if (checkbox.checked) {
+    checkbox.checked = false;
+  } else {
+    checkbox.checked = true;
+  }
+});
+
+loginForm.addEventListener("submit", handleLogin);
+
+function handleLogin(e) {
+  e.preventDefault();
+  const { login, pwd, check } = e.target;
+
+  console.log(login, pwd, check);
+  login.value = "";
+  pwd.value = "";
+  check.checked = false;
+}
